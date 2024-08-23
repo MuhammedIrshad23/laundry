bb41b1205fc547d5021170605e6756111ea09f89
MuhammedIrshad23 <irshadmuhammed418@gmail.com>
2024-08-23 16:17:08 +0530

    updated HomePage
    added profile image,search icon,title.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,10 +5,38 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-  appBar:AppBar(
-  
-  ),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 45),
+        child: Column(
+          children: [
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image.asset(
+                    'asset/man_face_image.png',
+                    height: 40,
+                    width: 40,
+                  ),
+                ),
+                const Icon(Icons.search),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              'Clean in one blink',
+              style: TextStyle(
+                  color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Text('data')
+          ],
+        ),
+      ),
     );
-  } 
+  }
 }
